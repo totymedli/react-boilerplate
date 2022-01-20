492589930b17cb03d27452407fb3926319995e96
totymedli <totymedli@keresztech.org>
2022-01-20 07:52:51 +0100

    Upgrade dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router, Switch, Route } from 'react-router-dom'
+import { Router, Routes, Route } from 'react-router-dom'
 
 import Home from 'scenes/Home/Home'
 
 export default ({ store, history }) => (
   <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
+    <Router location={history.location} navigator={history}>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </Router>
   </Provider>
 )
